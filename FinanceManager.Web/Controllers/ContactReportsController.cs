67d9b17da8d63d7b5c7d59310169532d70d11401
Muesli84 <m.stromberg@web.de>
2025-11-26 19:09:44 +0100

    ContactReportsController

--- FinanceManager.Web/Controllers/ContactReportsController.cs
+++ FinanceManager.Web/Controllers/ContactReportsController.cs
@@ -6,12 +6,14 @@ using FinanceManager.Shared.Dtos;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace FinanceManager.Web.Controllers;
 
 [ApiController]
 [Route("api/contacts/{contactId:guid}/aggregates")]
 [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Produces(MediaTypeNames.Application.Json)]
 public sealed class ContactReportsController : PostingReportsControllerBase
 {
     protected override PostingKind Kind => PostingKind.Contact;
@@ -19,6 +21,8 @@ public sealed class ContactReportsController : PostingReportsControllerBase
     public ContactReportsController(ICurrentUserService current, IPostingTimeSeriesService series) : base(current, series) { }
 
     [HttpGet]
+    [ProducesResponseType(typeof(IReadOnlyList<AggregatePointDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public Task<ActionResult<IReadOnlyList<AggregatePointDto>>> GetAsync(
         Guid contactId,
         [FromQuery] string period = "Month",
