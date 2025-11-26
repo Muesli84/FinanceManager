5005af853dbe7295774a7f758c7be4d1d2ff0075
Muesli84 <m.stromberg@web.de>
2025-11-26 19:33:42 +0100

    MetaHolidayProvidersController

--- FinanceManager.Web/Controllers/MetaHolidayCountriesController.cs
+++ FinanceManager.Web/Controllers/MetaHolidayCountriesController.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace FinanceManager.Web.Controllers;
 
 [ApiController]
 [Route("api/meta/holiday-countries")]
 [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Produces(MediaTypeNames.Application.Json)]
 public sealed class MetaHolidayCountriesController : ControllerBase
 {
     private static readonly string[] Countries = new[]
